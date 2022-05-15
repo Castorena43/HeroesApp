8ae12c41bcbf153d5c747945ff1bbdfe84e782d0
Castorena43 <hector.castorena.castillo@gmail.com>
2022-05-15 13:22:51 -0500

    [ADD] protected routes

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -7,7 +7,7 @@ export const HeroList = ({ publisher }) => {
   const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
 
   return (
-    <div className='row row-cols-3 row-cols-md-2 g-4 animate__animated animate__fadeIn'>
+    <div className='row row-cols-3 row-cols-md-2 row-cols-sm-1 g-4 animate__animated animate__fadeIn'>
       {
         heroes.map(heroe => (
           <HeroCard key={ heroe.id } { ...heroe } />
--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../auth/authContext';
+import { types } from '../../types/types';
 
 export const LoginScreen = () => {
 
   const navigate = useNavigate();
 
+  const { dispatch } = useContext(AuthContext);
+
   const handleLogin = () => {
-    navigate('/marvel', {
+    dispatch({
+      type: types.login,
+      payload: {
+        name: 'Hector'
+      }
+    })
+    navigate(localStorage.getItem('lastPath') || '/', {
       replace: true
     })
   }
